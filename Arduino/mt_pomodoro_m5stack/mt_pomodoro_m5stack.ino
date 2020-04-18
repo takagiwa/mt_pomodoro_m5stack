67803fe01db90207d9a665b3153aa78ba273ca2c
Masayuki Takagiwa <QYV03456@nifty.com>
2020-04-18 11:58:03 +0900

    Add first source for Arduino IDE

---
+++ Arduino/mt_pomodoro_m5stack/mt_pomodoro_m5stack.ino
@@ -0,0 +1,17 @@
+// define must ahead #include <M5Stack.h>
+#define M5STACK_MPU6886 
+// #define M5STACK_MPU9250 
+// #define M5STACK_MPU6050
+// #define M5STACK_200Q
+
+#include <M5Stack.h>
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
